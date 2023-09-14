b55151ec5f2ef857c6e0da39af73d6378bce8986
Alunara <>
2023-09-14 23:42:32 +0200

    New release

--- assets/index-c62d22f1.js
+++ assets/index-c62d22f1.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-db860f93.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./vendor-9133d749.js";/**
+import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-76124cfc.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./vendor-2c3eeb85.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
