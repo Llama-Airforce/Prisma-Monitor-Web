eb8e3d56f7b38c14e47af877004b2a3ee1e75e58
Alunara <>
2023-11-13 18:25:56 +0100

    New release

--- assets/index-c04225e8.js
+++ assets/index-c04225e8.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-cffde46c.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./vendor-54b60f3b.js";/**
+import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-59c460ce.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./vendor-d50ff160.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
