a2f3f2af234017b7459744cc01f8fcf55e22d87c
Alunara <>
2024-05-31 16:00:11 +0200

    New release

--- assets/index-zTGWXe8r.js
+++ assets/index-zTGWXe8r.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-Df6cbeh5.js";import"./vendor-By8edvmC.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-CNwCBMFc.js";import"./vendor-C54j3EYj.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
