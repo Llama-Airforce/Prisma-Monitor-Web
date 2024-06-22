9facec27b0911245cab8eab9ef4c02ce8c3474e4
Alunara <>
2024-06-23 00:05:58 +0200

    New release

--- assets/index-BXsAxoZ0.js
+++ assets/index-BXsAxoZ0.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-DM_3008S.js";import"./vendor-D7EQgE-M.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-euM2H7Qf.js";import"./vendor-CdMkTzUC.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
