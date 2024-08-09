4047193f103a174cb3e384d42ba32a03982872c1
Alunara <>
2024-08-09 20:13:04 +0200

    New release

--- assets/index-Bhi_B1GQ.js
+++ assets/index-Bhi_B1GQ.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-euM2H7Qf.js";import"./vendor-CdMkTzUC.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-w0wXNp0C.js";import"./vendor-BH5-G0ad.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
