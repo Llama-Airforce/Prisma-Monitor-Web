432923dbd0efadf2874e6f13c5492cbb7c3e39aa
Alunara <>
2024-08-21 18:39:37 +0200

    New release

--- assets/index-BnsfdyhX.js
+++ assets/index-BnsfdyhX.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-Df7tqArP.js";import"./vendor-ngDrVcqS.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-CH-T-nzI.js";import"./vendor-CuCaqR8k.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
